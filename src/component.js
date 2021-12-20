8195c95fcf45704a0653a7e09271dd3e024f782a
Dmitry Basov <dmbasov@gmail.com>
2021-12-20 04:59:56 +0300

    use unique ids for patterns

--- src/component.js
+++ src/component.js
@@ -130,13 +130,13 @@ class _VizabiLineChart extends BaseComponent {
               </filter-->
 
           </g>
-          <rect class="vzb-lc-forecastoverlay vzb-hidden" x="0" y="0" width="100%" height="100%" fill="url(#vzb-lc-pattern-lines)" pointer-events='none'></rect>
+          <rect class="vzb-lc-forecastoverlay vzb-hidden" x="0" y="0" width="100%" height="100%" fill="url(#vzb-lc-pattern-lines-${config.id})" pointer-events='none'></rect>
           <g class="vzb-datawarning-button vzb-noexport"></g>
       </svg>
       <div class="vzb-tooltip vzb-hidden"></div>
       <svg>
         <defs>
-            <pattern id="vzb-lc-pattern-lines" x="0" y="0" patternUnits="userSpaceOnUse" width="50" height="50" viewBox="0 0 10 10"> 
+            <pattern id="vzb-lc-pattern-lines-${config.id}" x="0" y="0" patternUnits="userSpaceOnUse" width="50" height="50" viewBox="0 0 10 10"> 
                 <path d='M-1,1 l2,-2M0,10 l10,-10M9,11 l2,-2' stroke='black' stroke-width='3' opacity='0.08'/>
             </pattern> 
         </defs>
